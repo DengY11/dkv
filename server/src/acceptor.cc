4f7fff107703d64a74ea688ac803586e5e50a471
DengY11 <212294929@qq.com>
2026-01-16 00:10:49 +0800

    error info

--- server/src/acceptor.cc
+++ server/src/acceptor.cc
@@ -9,6 +9,7 @@
 #include <cerrno>
 #include <cstdint>
 #include <iostream>
+#include <system_error>
 #include <stdexcept>
 #include <string>
 #include <thread>
@@ -23,7 +24,10 @@ namespace dkv_server {
 struct Acceptor::Impl {
   Impl(std::string bind, int port, std::vector<SubReactor*> subs) : bind_(std::move(bind)), port_(port), subs_(std::move(subs)) {}
 
-  void Start() { thread_ = std::thread([this] { Loop(); }); }
+  void Start() {
+    Setup();
+    thread_ = std::thread([this] { Loop(); });
+  }
 
   void Stop() {
     stopping_.store(true, std::memory_order_relaxed);
@@ -31,29 +35,37 @@ struct Acceptor::Impl {
   }
 
  private:
+  void Setup() {
+    listen_fd_.reset(::socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK | SOCK_CLOEXEC, 0));
+    if (!listen_fd_.valid()) ThrowSys("socket");
+    SetReuseAddr(listen_fd_.get());
+    SetKeepAlive(listen_fd_.get());
+
+    sockaddr_in addr{};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(static_cast<uint16_t>(port_));
+    if (::inet_pton(AF_INET, bind_.c_str(), &addr.sin_addr) != 1) {
+      throw std::runtime_error("invalid bind address: " + bind_);
+    }
+    if (::bind(listen_fd_.get(), reinterpret_cast<sockaddr*>(&addr), sizeof(addr)) < 0) {
+      int err = errno;
+      throw std::system_error(err, std::generic_category(), "bind(" + bind_ + ":" + std::to_string(port_) + ")");
+    }
+    if (::listen(listen_fd_.get(), SOMAXCONN) < 0) {
+      int err = errno;
+      throw std::system_error(err, std::generic_category(), "listen(" + bind_ + ":" + std::to_string(port_) + ")");
+    }
+
+    epoll_fd_.reset(::epoll_create1(EPOLL_CLOEXEC));
+    if (!epoll_fd_.valid()) ThrowSys("epoll_create1");
+    epoll_event ev{};
+    ev.events = EPOLLIN;
+    ev.data.fd = listen_fd_.get();
+    if (::epoll_ctl(epoll_fd_.get(), EPOLL_CTL_ADD, listen_fd_.get(), &ev) < 0) ThrowSys("epoll_ctl(ADD listen)");
+  }
+
   void Loop() {
     try {
-      listen_fd_.reset(::socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK | SOCK_CLOEXEC, 0));
-      if (!listen_fd_.valid()) ThrowSys("socket");
-      SetReuseAddr(listen_fd_.get());
-      SetKeepAlive(listen_fd_.get());
-
-      sockaddr_in addr{};
-      addr.sin_family = AF_INET;
-      addr.sin_port = htons(static_cast<uint16_t>(port_));
-      if (::inet_pton(AF_INET, bind_.c_str(), &addr.sin_addr) != 1) {
-        throw std::runtime_error("invalid bind address: " + bind_);
-      }
-      if (::bind(listen_fd_.get(), reinterpret_cast<sockaddr*>(&addr), sizeof(addr)) < 0) ThrowSys("bind");
-      if (::listen(listen_fd_.get(), SOMAXCONN) < 0) ThrowSys("listen");
-
-      epoll_fd_.reset(::epoll_create1(EPOLL_CLOEXEC));
-      if (!epoll_fd_.valid()) ThrowSys("epoll_create1");
-      epoll_event ev{};
-      ev.events = EPOLLIN;
-      ev.data.fd = listen_fd_.get();
-      if (::epoll_ctl(epoll_fd_.get(), EPOLL_CTL_ADD, listen_fd_.get(), &ev) < 0) ThrowSys("epoll_ctl(ADD listen)");
-
       std::vector<epoll_event> events(16);
       while (!stopping_.load(std::memory_order_relaxed)) {
         int n = ::epoll_wait(epoll_fd_.get(), events.data(), static_cast<int>(events.size()), 1000);
--- server/src/server.cc
+++ server/src/server.cc
@@ -19,34 +19,39 @@ DkvServer::~DkvServer() { Stop(); }
 void DkvServer::Start() {
   if (started_.exchange(true)) return;
 
-  dkv::Options opt = cfg_.dkv_options;
-  std::unique_ptr<dkv::DB> db;
-  dkv::Status s = dkv::DB::Open(opt, db);
-  if (!s.ok()) throw std::runtime_error("dkv::DB::Open failed: " + s.ToString());
-  db_ = std::move(db);
+  try {
+    dkv::Options opt = cfg_.dkv_options;
+    std::unique_ptr<dkv::DB> db;
+    dkv::Status s = dkv::DB::Open(opt, db);
+    if (!s.ok()) throw std::runtime_error("dkv::DB::Open failed: " + s.ToString());
+    db_ = std::move(db);
 
-  std::size_t sub_n = cfg_.subreactors;
-  if (sub_n == 0) sub_n = std::max<std::size_t>(1, std::thread::hardware_concurrency());
-  std::size_t worker_n = cfg_.workers;
-  if (worker_n == 0) worker_n = std::max<std::size_t>(1, std::thread::hardware_concurrency());
-  cfg_.subreactors = sub_n;
-  cfg_.workers = worker_n;
+    std::size_t sub_n = cfg_.subreactors;
+    if (sub_n == 0) sub_n = std::max<std::size_t>(1, std::thread::hardware_concurrency());
+    std::size_t worker_n = cfg_.workers;
+    if (worker_n == 0) worker_n = std::max<std::size_t>(1, std::thread::hardware_concurrency());
+    cfg_.subreactors = sub_n;
+    cfg_.workers = worker_n;
 
-  workers_ = std::make_unique<ThreadPool>(worker_n);
-  subreactors_.reserve(sub_n);
-  std::vector<SubReactor*> subs_raw;
-  subs_raw.reserve(sub_n);
-  for (std::size_t i = 0; i < sub_n; ++i) {
-    subreactors_.push_back(std::make_unique<SubReactor>(i, db_.get(), &cfg_, workers_.get()));
-    subs_raw.push_back(subreactors_.back().get());
+    workers_ = std::make_unique<ThreadPool>(worker_n);
+    subreactors_.reserve(sub_n);
+    std::vector<SubReactor*> subs_raw;
+    subs_raw.reserve(sub_n);
+    for (std::size_t i = 0; i < sub_n; ++i) {
+      subreactors_.push_back(std::make_unique<SubReactor>(i, db_.get(), &cfg_, workers_.get()));
+      subs_raw.push_back(subreactors_.back().get());
+    }
+    for (auto& sr : subreactors_) sr->Start();
+
+    acceptor_ = std::make_unique<Acceptor>(cfg_.bind, cfg_.port, std::move(subs_raw));
+    acceptor_->Start();
+
+    std::cout << "dkv-server listening on " << cfg_.bind << ":" << cfg_.port << " (subreactors=" << sub_n
+              << ", workers=" << worker_n << ", data_dir=" << cfg_.dkv_options.data_dir.string() << ")\n";
+  } catch (...) {
+    Stop();
+    throw;
   }
-  for (auto& sr : subreactors_) sr->Start();
-
-  acceptor_ = std::make_unique<Acceptor>(cfg_.bind, cfg_.port, std::move(subs_raw));
-  acceptor_->Start();
-
-  std::cout << "dkv-server listening on " << cfg_.bind << ":" << cfg_.port << " (subreactors=" << sub_n
-            << ", workers=" << worker_n << ", data_dir=" << cfg_.dkv_options.data_dir.string() << ")\n";
 }
 
 void DkvServer::Stop() {
@@ -56,7 +61,11 @@ void DkvServer::Stop() {
     if (sr) sr->Stop();
   }
   if (workers_) workers_->Stop();
+
+  acceptor_.reset();
+  subreactors_.clear();
+  workers_.reset();
+  db_.reset();
 }
 
 }  // namespace dkv_server
-
